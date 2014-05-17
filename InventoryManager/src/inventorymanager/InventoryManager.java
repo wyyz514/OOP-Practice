2aa282fe048491019499215e0d1cb436a38c8845
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2014-05-17 01:21:03 -0400

    Added some live testing to see if null pointers are avoided. Not sure why the .equals method doesn't work but == seems to work fine for comparison

--- InventoryManager/src/inventorymanager/InventoryManager.java
+++ InventoryManager/src/inventorymanager/InventoryManager.java
@@ -2,9 +2,13 @@ package inventorymanager;
 
 
 public class InventoryManager {
-
+Inventory inv;
+Item item;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		InventoryManager im = new InventoryManager();
+		im.inv = new Inventory();
+		im.item = null;
+		im.inv.saveItem(im.item);	
 	}
 }
