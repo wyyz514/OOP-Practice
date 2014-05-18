ce1db548da01b8bdc6a4c4241dd661233766890f
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2014-05-17 20:26:02 -0400

    Remade null value test

--- InventoryManager/src/inventorymanager/InventoryManagerTests.java
+++ InventoryManager/src/inventorymanager/InventoryManagerTests.java
@@ -9,12 +9,11 @@ public class InventoryManagerTests {
 	@Test
 	public void nullValuesTest()
 	{
-		Item testItem = null;
-		Inventory testInventory = new Inventory();
-		int capacityBefore = Inventory.getInventorySize(); //save current inventory size to see if it changes after null value is inserted
-		testInventory.saveItem(testItem);
-		//test to see if the capacity changes after addition of a null value. It should not
-		assertTrue(capacityBefore == Inventory.getInventorySize());
+		InventoryManager.createInventory();
+		Inventory inventory = InventoryManager.getInventory();
+		Item item = null;
+		InventoryManager.saveItem(item, inventory);
+		assertTrue(inventory.getInventorySize() == 0);
 	}
 
 }
