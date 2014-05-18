1ac7ff2bdfe225940a0bbeaecd051c024724c268
WoleObayomi <oluwole.obayomi@mail.mcgill.ca>
2014-05-17 20:24:51 -0400

    Included add method so save can be ddone by InventoryManager

--- InventoryManager/src/inventorymanager/Inventory.java
+++ InventoryManager/src/inventorymanager/Inventory.java
@@ -3,37 +3,23 @@ package inventorymanager;
 public class Inventory {
 	final int MAX_INVENTORY_SIZE = 100;
 	private Item[]items; //has many relationship
-	private static int inventorySize;
-	
+	public static int inventorySize = 0;
 	public Inventory()
 	{
 		items = new Item[MAX_INVENTORY_SIZE];
-		inventorySize = 0;
 	}
 	
-	public void saveItem(Item item)
+	public void add(Item item)
 	{
-		if(inventorySize == 100) //if inventory is at capacity, do not save item
-		{
-			System.out.println("Inventory is already full. Cannot add new items");
-			return;
-		}
-		else
-		{
-			if(item != null)
-			{
-				items[inventorySize] = item;
-				inventorySize++;
-			}
-		}
+		items[inventorySize] = item;
 	}
 	
-	public static int getInventorySize()
+	public int getInventorySize()
 	{
 		return inventorySize;
 	}
 	
-	public Item[] getInventory()
+	public Item[] getItems()
 	{
 		return items;
 	}
